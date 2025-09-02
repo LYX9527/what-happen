6454fc1b4cf85e46ac9c646303102eeab36d9fc8
LYX <lyxing9527@163.com>
2025-09-02 14:49:34 +0800

    feat: 更新 nuxt 配置，新增多语言支持及结构化数据以提升 SEO

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,14 +17,7 @@ export default defineNuxtConfig({
 
     modules: ['@nuxt/ui', 'shadcn-nuxt'],
     shadcn: {
-        /**
-         * Prefix for all the imported component
-         */
         prefix: '',
-        /**
-         * Directory that the component lives in.
-         * @default "./components/ui"
-         */
         componentDir: './components/ui'
     },
     css: ['~/assets/css/tailwind.css', '~/assets/css/base.css'],
@@ -33,18 +26,51 @@ export default defineNuxtConfig({
             tailwindcss(),
         ],
     },
-    app:{
-        head:{
-            link:[
-                { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'https://cdn.yltfspace.com/16.png' },
-                { rel: 'icon', type: 'image/png', sizes: '32x32', href: 'https://cdn.yltfspace.com/32.png' },
-                { rel: 'icon', type: 'image/png', sizes: '64x64', href: 'https://cdn.yltfspace.com/64.png' },
-                { rel: 'icon', type: 'image/png', sizes: '256x256', href: 'https://cdn.yltfspace.com/256.png' },
-                { rel: 'icon', type: 'image/png', sizes: '512x512', href: 'https://cdn.yltfspace.com/512.png' },
-                { rel: 'apple-touch-icon', sizes: '120x120', href: 'https://cdn.yltfspace.com/512.png' },
-                { rel: 'apple-touch-icon', sizes: '152x152', href: 'https://cdn.yltfspace.com/512.png' },
-                { rel: 'apple-touch-icon', sizes: '167x167', href: 'https://cdn.yltfspace.com/512.png' },
-                { rel: 'apple-touch-icon', sizes: '180x180', href: 'https://cdn.yltfspace.com/512.png' },
+    app: {
+        head: {
+            htmlAttrs: {
+                lang: "zh-CN"
+            },
+            meta: [
+                {
+                    property: "og:site:name",
+                    content: "今日时事"
+                }
+            ],
+            script: [
+                {
+                    type: "application/ld+json",
+                    textContent: `{
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "今日时事",
+  "url": "https://news.yltfspace.com",
+  "alternateName":["今日新闻","新闻","时事","热点","今日热点","今日新闻热点","最新新闻","最新时事","最新热点"],
+  "description": "每日更新全球最新鲜、最有趣的新闻资讯，涵盖科技、娱乐、体育、国际等多个领域，让你轻松掌握世界动态。",
+  "inLanguage": "zh-CN",
+  "keywords": "新闻, 时事, 热点, 今日新闻, 今日时事, 最新新闻, 最新时事, 最新热点",
+  "publisher": {
+    "@type": "Person",
+    "name": "Yltf",
+    "url": "https://blog.yltfspace.com",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://cdn.yltfspace.com/512.png"
+    }
+  }
+}`
+                }
+            ],
+            link: [
+                {rel: 'icon', type: 'image/png', sizes: '16x16', href: 'https://cdn.yltfspace.com/16.png'},
+                {rel: 'icon', type: 'image/png', sizes: '32x32', href: 'https://cdn.yltfspace.com/32.png'},
+                {rel: 'icon', type: 'image/png', sizes: '64x64', href: 'https://cdn.yltfspace.com/64.png'},
+                {rel: 'icon', type: 'image/png', sizes: '256x256', href: 'https://cdn.yltfspace.com/256.png'},
+                {rel: 'icon', type: 'image/png', sizes: '512x512', href: 'https://cdn.yltfspace.com/512.png'},
+                {rel: 'apple-touch-icon', sizes: '120x120', href: 'https://cdn.yltfspace.com/512.png'},
+                {rel: 'apple-touch-icon', sizes: '152x152', href: 'https://cdn.yltfspace.com/512.png'},
+                {rel: 'apple-touch-icon', sizes: '167x167', href: 'https://cdn.yltfspace.com/512.png'},
+                {rel: 'apple-touch-icon', sizes: '180x180', href: 'https://cdn.yltfspace.com/512.png'},
             ]
         }
     }
